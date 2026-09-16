51a7fffd442b7ad8658cc01b191b534d8953ddbf
justfunandenjoy1-lab <justfunandenjoy1@gmail.com>
2026-09-16 08:54:57 +0600

    Create Color.kt

---
+++ app/src/main/java/com/variaflow/mobile/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.variaflow.mobile.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Cyan80 = Color(0xFF80DEEA)
+val CyanGrey80 = Color(0xFFB2DFDB)
+val Cyan40 = Color(0xFF00ACC1)
+val CyanGrey40 = Color(0xFF00897B)
+val DarkBackground = Color(0xFF0C1013)
+val DarkSurface = Color(0xFF161C20)
+val DarkSurfaceVariant = Color(0xFF20272B)
+val ElectricCyan = Color(0xFF00E5FF)
+val WarningAmber = Color(0xFFFFB300)
+val ErrorRed = Color(0xFFFF5252)
