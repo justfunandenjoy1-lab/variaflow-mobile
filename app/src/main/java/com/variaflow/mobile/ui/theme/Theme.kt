0522a19836857ab3ba9d09dc28fa31ca605edc68
justfunandenjoy1-lab <justfunandenjoy1@gmail.com>
2026-09-16 14:47:14 +0600

    Update Theme.kt

--- app/src/main/java/com/variaflow/mobile/ui/theme/Theme.kt
+++ app/src/main/java/com/variaflow/mobile/ui/theme/Theme.kt
@@ -43,9 +43,11 @@ fun VariaFlowTheme(
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.background.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+            val activity = view.context as? Activity
+            activity?.window?.let { window ->
+                window.statusBarColor = colorScheme.background.toArgb()
+                WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+            }
         }
     }
     MaterialTheme(colorScheme = colorScheme, typography = Typography, content = content)
